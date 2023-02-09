f10a9a8a4c964eb0c7b34a2560d27be74a734837
malcomowilla <owillamalcom@gmail.com>
2023-02-09 09:36:58 +0300

    control-flow-loops

--- looping.rb
+++ looping.rb
@@ -1,5 +1,13 @@
+# #happy_new_year puts out a countdown from 10 to 1, followed by "Happy New Year!
+
 def happy_new_year
   # your code here
+  counter = 10
+  until counter == 0
+    puts counter
+    counter -= 1
+end
+puts "Happy New Year!"
 end
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
@@ -15,10 +23,35 @@ def fizzbuzz(num)
   end
 end
 
-def fizzbuzz_printer
-  # your code here
-end
+# #fizzbuzz_printer uses `puts` to print numbers from 1 to 100, replacing multiples of 3 and 5 with Fizz and Buzz
+  def fizzbuzz_printer
+    # your code here
+    (1..100).each do |num|
+      puts fizzbuzz(num)
+  end
+  end
 
+
+  # #reverse_string reverses a string
 def reverse_string(str)
   # your code here
+  str_reversed = ""
+  str.length.times do |i|
+    str_reversed = str[i] + str_reversed
 end
+str_reversed
+end
+
+
+# 10.times do |i|
+
+#   puts "Looping"
+#   puts "i is:#{i}"
+# end
+
+# 10.times {|i| puts "i is: #{i}"}
+
+
+# (1..20).each do |num|
+#   puts num
+# end
